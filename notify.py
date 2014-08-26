2888e04e07e16ba2323f5f30c28d0fdc16d6b2d9
James Douglass <jamesdouglassusa@gmail.com>
2014-08-25 21:07:41 -0700

    Initial commit of notification script.
    Currently fetches page, finds the open-positions div and extracting the
    jobs.

---
+++ notify.py
@@ -0,0 +1,13 @@
+import urllib2
+
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    jobs_html_doc = urllib2.urlopen('https://github.com/about/jobs').read()
+    jobs_soup = BeautifulSoup(jobs_html_doc)
+
+    open_positions = jobs_soup.find('div', 'jobs-open-positions').find_all('a')
+    jobs = sorted([{'name': job.string, 'link': job['href']} for job in open_positions])
+    print jobs
+
+
